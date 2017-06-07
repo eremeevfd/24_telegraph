37b121f4c5953459c6829767cbe4582498ef788b
eremeevfd <dartfedor@mail.ru>
2017-06-07 09:00:29 +0300

    fix for deploy to heroku

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__) + '/')
 db_dir = os.path.abspath(os.path.join(basedir, 'app.db'))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_dir
+SQLALCHEMY_DATABASE_URI = 'postgresql:///' + db_dir
 # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
