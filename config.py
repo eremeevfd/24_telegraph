fdd91642f4e0765b3cbb9a266c9b679662dadfba
eremeevfd <dartfedor@mail.ru>
2017-06-07 14:43:24 +0300

    connecting pg for heroku

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ SECRET_KEY = 'Glavnoe - eto funk v dushe, rebyata'
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__) + '/')
-db_dir = os.path.abspath(os.path.join(basedir, 'app.db'))
+# db_dir = os.path.abspath(os.path.join(basedir, 'app.db'))
 
-SQLALCHEMY_DATABASE_URI = 'postgresql:///' + db_dir
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
