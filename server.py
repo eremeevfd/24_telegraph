e30b7c1077e033cd2266fcf7e9a23304d8dcff6a
eremeevfd <dartfedor@mail.ru>
2017-06-07 15:32:30 +0300

    realised cookie auth

--- server.py
+++ server.py
@@ -22,12 +22,14 @@ class Article(db.Model):
     signature = db.Column(db.String(80))
     body = db.Column(db.String)
     url = db.Column(db.String)
+    cookie = db.Column(db.String)
 
-    def __init__(self, header, signature, body, url):
+    def __init__(self, header, signature, body, url, cookie):
         self.header = header
         self.signature = signature
         self.body = body
         self.url = url
+        self.cookie = cookie
 
     def __repr__(self):
         return 'Article %r' % self.header
@@ -43,12 +45,14 @@ def form():
         url = transliterate(request.form['header'])
         url = url + '-{month}-{day}'.format(month=datetime.date.today().month,
                                             day=datetime.date.today().day)
+        cookie = request.cookies.get('session')
         articles_count = Article.query.filter_by(header=header).count()
         if articles_count != 0:
             url = url + '-{article_counter}'.format(article_counter=articles_count+1)
-        new_article = Article(header, signature, body, url)
+        new_article = Article(header, signature, body, url, cookie)
         db.session.add(new_article)
         db.session.commit()
+        # print(request.cookies.get('session'))
         return redirect(url_for('article', article_url=url))
 
     return render_template('form.html')
@@ -57,7 +61,8 @@ def form():
 @app.route('/<article_url>', methods=['GET', 'POST'])
 def article(article_url):
     open_article = Article.query.filter_by(url=article_url).first()
-    return render_template('article.html', article=open_article)
+    cookie = request.cookies.get('session')
+    return render_template('article.html', article=open_article, cookie=cookie)
 
 
 if __name__ == "__main__":
