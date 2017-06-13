91c0f8363fd16a58c9d77cfdd1371f259c1133e7
eremeevfd <dartfedor@mail.ru>
2017-06-13 17:09:28 +0300

    fixed config to match db url or just sqlite, fixed html

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__) + '/')
 db_dir = os.path.abspath(os.path.join(basedir, 'app.db'))
 
-# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_dir
-# SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+if 'DATABASE_URL' in os.environ:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_dir
 SQLALCHEMY_TRACK_MODIFICATIONS = False
