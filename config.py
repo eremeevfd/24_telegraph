dfeff923e0e26a6818ff7cf59fc8dc5e50923dcb
eremeevfd <dartfedor@mail.ru>
2017-06-14 10:49:52 +0300

    fix global in config

--- config.py
+++ config.py
@@ -6,8 +6,5 @@ basedir = os.path.abspath(os.path.dirname(__file__) + '/')
 db_dir = os.path.abspath(os.path.join(basedir, 'app.db'))
 
 
-if 'DATABASE_URL' in os.environ:
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-else:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_dir
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + db_dir
 SQLALCHEMY_TRACK_MODIFICATIONS = False
